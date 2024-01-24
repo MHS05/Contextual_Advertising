0d335bb0ca0282d84c01a640c7ae7cd0bf2cbdcd
skyup8012 <156740464+skyup8012@users.noreply.github.com>
2024-01-24 10:00:39 +0900

    sql문 만들어봄

--- MHS05/Document/mhs.sql
+++ MHS05/Document/mhs.sql
@@ -83,3 +83,75 @@ create table reply
 	foreign key(uno) references user(uno)
 ) comment '댓글';
 
+create table news
+(
+	nno int auto_increment primary key comment '뉴스번호',
+	category varchar(2) comment '카데고리',
+	title varchar(200) comment '제목',
+	note text comment '내용',
+	writer varchar(10) comment '작성자',
+	wdate datetime default now() comment '작성일',
+	image varchar(100) comment '이미지_논리',
+	phyimage varchar(100) comment '이미지_물리',
+	emotion varchar(5) comment '긍부정'
+	adno int comment '광고번호'
+	foreign key(adno) references ad(adno)
+) comment '건강소식';
+
+create table newskeyword
+(
+	nkeyno int auto_increment primary key comment '뉴스키워드관리번호',
+	nno int comment '뉴스번호',
+	nkey varchar(10) comment '뉴스키워드',
+	nkeynum int comment '키워드빈도스',
+	foreign key(nno) references news(nno)
+) comment '뉴스키워드';
+
+create table semotion
+(
+	senno int auto_increment primary key comment '문장별관리번호',
+	nno int comment '뉴스번호',
+	sentance varchar(100) comment '문장',
+	emotion varchar(10) comment '긍부정여부',
+	score varchar(10) comment '긍부정점수',
+	foreign key(nno) references news(nno)
+) comment '문장별긍부정점수';
+
+create table ad
+(
+	adno int auto_increment primary key comment '광고번호',
+	adname varchar(200) comment '상품이름',
+	image varchar(100) comment '이미지_논리',
+	phyimage varchar(100) comment '이미지_물리',
+) comment '광고';
+
+create table adkeyword
+(
+	adkeyno int auto_increment primary key comment '광고키워드관리번호',
+	adno int comment '광고번호',
+	adkey varchar(10) comment '광고키워드',
+	adkeynum int comment '키워드빈도수',
+	foreign key(adno) references ad(adno)
+) comment '광고키워드';
+
+create table clickad
+(
+	clickadno int auto_increment primary key comment '노출정보관리번호',
+	adno int comment '광고번호',
+	cdate datetime comment '노출일',
+	cnews int comment '노출게시물번호',
+	foreign key(adno) references ad(adno)
+) comment '광고키워드';
+
+create table similarity
+(
+	sno int auto_increment primary key comment '유사도관리번호',
+	adkeyno int comment '광고키워드관리번호',
+	nkeyno int comment '뉴스키워드관리번호',
+	nkey varchar(10) comment '뉴스키워드',
+	adkey varchar(10) comment '광고키워드',
+	adno int comment '광고번호',
+	nno int comment '뉴스키워드',
+	foreign key(adkeyno) references adkeyword(adkeyno)
+	foreign key(nkeyno) references newskeyword(nkeyno)
+) comment '광고키워드';
