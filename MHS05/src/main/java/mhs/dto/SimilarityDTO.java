85f973a1b55da0dac6429c056c0bf1265de2cb23
usernameHoon <92309206+usernameHoon@users.noreply.github.com>
2024-01-29 15:14:34 +0900

    하이차트 하위 파일 삭제 및 수정본

--- MHS05/src/main/java/mhs/dto/SimilarityDTO.java
+++ MHS05/src/main/java/mhs/dto/SimilarityDTO.java
@@ -1,10 +1,43 @@
 package mhs.dto;
 
+import java.util.ArrayList;
+
 import mhs.dao.*;
 import mhs.vo.*;
 
 public class SimilarityDTO extends DBManager
-{
+{	
+	public ArrayList<SimilarityVO> GetList(String nno)
+	{
+		ArrayList<SimilarityVO> list = new ArrayList<SimilarityVO>();
+		
+		this.DBOpen();
+		
+		String sql = "";
+		
+		sql  = "select sno, adkeyno, nkeyno, nkey, adkey, adno ";
+		sql += "from similarity ";
+		sql += "where nno = '" + nno + "' ";
+		//작성일 내림차순
+		sql += "order by sno ";
+		this.RunSelect(sql);
+		while( this.GetNext() == true)
+		{
+			SimilarityVO vo = new SimilarityVO();
+			vo.setSno(this.GetValue("sno"));
+			vo.setAdkeyno(this.GetValue("adkeyno"));
+			vo.setNkeyno(this.GetValue("nkeyno"));
+			vo.setNkey(this.GetValue("nkey"));
+			vo.setAdkey(this.GetValue("adkey"));
+			vo.setAdno(this.GetValue("adno"));
+			
+			list.add(vo);
+		}		
+		this.DBClose();
+		
+		return list;		
+	}
+	
 	public SimilarityVO Read(String nno)
 	{
 		String sql = "";
